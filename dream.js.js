c5d41d74bcf4ed3946bcb65a2b2da13141ab31d3
TorpeDoSyS <45240133+TorpeDoSyS@users.noreply.github.com>
2018-12-29 14:46:26 +0200

    Add files via upload

---
+++ dream.js.js
@@ -0,0 +1,17 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Happy New Year`,"http://twitch.tv/Dream")
+client.user.setStatus("#IT WORKS!!")
+});
+
+
+
+client.login("NDE5NTk3NjMwOTE0NjkxMDgy.DvZDpg.ZwhXMYPrWQ86JZzah7NfOFN4d14");// لا تغير فيها شيء
